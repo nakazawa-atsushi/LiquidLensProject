d0e61e50bb45ff4a109a9286d888448b21b31f5b
nakazawa-atsushi <nakazawa.atsushi@gmail.com>
2024-10-11 09:16:58 +0900

    update

--- focusestimate.py
+++ focusestimate.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
-# In[1]:
-
-
+# +
 # referenced from https://qiita.com/takubb/items/7d45ae701390912c7629
 import torch
 import torch.nn as nn
@@ -28,25 +38,11 @@ from tqdm.notebook import tqdm  # jupyter で実行するとき
 from models import RESNETLIKE, MyModel, MyModel_shallow
 
 
-# In[2]:
-
-
-# リソースの指定（CPU/GPU）
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-# In[3]:
-
-
-# データセットの作成
-trans = transforms.Compose([
-    transforms.ToTensor()
-])
+# -
 
 class FocusPatchDataset(torch.utils.data.Dataset):
     # FILEはデータが入っていたファイル、imsize = 画像サイズ、psize = パッチサイズ
     def __init__(self, DIR, imsize, psize, channels, DEPTH_GAP, n_sample, transforms):
-        
         # ground truth depthを読み込むdd
         with open(os.path.join(DIR,'depth.pkl'),"rb") as f:
             self.gt = pickle.load(f)
@@ -82,6 +78,9 @@ class FocusPatchDataset(torch.utils.data.Dataset):
             self.locs.append([xx,yy])
 
         print('locations', self.locs)
+
+        # 学習高速化のためバッファを用意する
+        self.buffer = {}
     
     def __len__(self):
         length = (len(self.fvalues) - self.DEPTH_GAP)*self.n_sample
@@ -99,10 +98,13 @@ class FocusPatchDataset(torch.utils.data.Dataset):
         
         val = float(self.gt[y,x]) - float(self.fvalues[n])
 
+        # もしバッファにデータがあるならそれを返す
+        if index in self.buffer.keys():
+            return self.transforms(self.buffer[index]), val
+
+        # バッファにない場合はファイルから読み出す
         FILE1 = os.path.join(self.DIR,f'{self.fvalues[n]:04d}.bmp')
         FILE2 = os.path.join(self.DIR,f'{self.fvalues[n+self.DEPTH_GAP]:04d}.bmp')
-
-        # print(self.gt[y,x], self.fvalues[n], val, FILE1, FILE2, x, y)
         
         img = Image.open(FILE1)
         img = img.crop((x, y, x+self.psize, y+self.psize))
@@ -119,84 +121,11 @@ class FocusPatchDataset(torch.utils.data.Dataset):
         imgs.append(img)
         
         out = np.stack(imgs,axis=2)
-        #print(out.shape)
+
+        # バッファーに保存する
+        self.buffer[index] = out
         
         return self.transforms(out), val
-
-
-# In[4]:
-
-
-PSIZE = 65
-DEPTH_GAP = 100
-N_SAMPLE = 20
-
-# merge several dataset together
-dataset1 = FocusPatchDataset(DIR = 'data/202410041703', imsize = [1600,1200], 
-                            psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
-                            DEPTH_GAP=DEPTH_GAP, transforms = trans)
-dataset2 = FocusPatchDataset(DIR = 'data/202410041747', imsize = [1600,1200], 
-                            psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
-                            DEPTH_GAP=DEPTH_GAP, transforms = trans)
-dataset3 = FocusPatchDataset(DIR = 'data/202410080808', imsize = [1600,1200], 
-                            psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
-                            DEPTH_GAP=DEPTH_GAP, transforms = trans)
-dataset4= FocusPatchDataset(DIR = 'data/202410080819', imsize = [1600,1200], 
-                            psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
-                            DEPTH_GAP=DEPTH_GAP, transforms = trans)
-
-dataset = torch.utils.data.ConcatDataset([dataset1,dataset2,dataset3,dataset4])
-
-
-# In[5]:
-
-
-train_set, test_set = torch.utils.data.random_split(dataset, [0.7,0.3])
-len(dataset)
-
-
-# In[6]:
-
-
-# データローダーの作成
-train_loader = torch.utils.data.DataLoader(train_set,
-                                           batch_size=256,  # バッチサイズ
-                                           shuffle=True,  # データシャッフル
-                                           num_workers=5,  # 高速化
-                                           pin_memory=True  # 高速化                                      
-                                           )
-test_loader = torch.utils.data.DataLoader(test_set,
-                                          batch_size=256,
-                                          shuffle=True,
-                                          num_workers=5,  # 高速化
-                                          pin_memory=True  # 高速化                                                   
-                                          )
-
-
-# In[7]:
-
-
-# モデル・損失関数・最適化アルゴリスムの設定
-LOAD_WEIGHT = True
-# model = RESNETLIKE(channels=2).to(device)
-model = MyModel(channels=2)
-#model = MyModel_shallow(channels=2).to(device)
-weight_file = f"weights/weight_{PSIZE}_{DEPTH_GAP}.pth"
-
-if LOAD_WEIGHT == True:
-    # 学習済みモデルのロード
-    print("load weight", weight_file)
-    model.load_state_dict(torch.load(weight_file))
-    model.eval()
-
-model = model.to(device)
-
-criterion = nn.L1Loss()
-optimizer = optim.Adam(model.parameters())
-#optimizer = optim.SGD(model.parameters())
-
-
-# In[8]:
 
 
 # モデル訓練関数
@@ -235,10 +164,73 @@ def train_model(model, train_loader, test_loader):
     return model, np.mean(train_batch_loss), np.mean(test_batch_loss)
 
 
-# In[ ]:
-
-
 if __name__ == "__main__":
+    # リソースの指定（CPU/GPU）
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    # データセットの作成
+    trans = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    
+    # パラメータの設定
+    PSIZE = 65
+    DEPTH_GAP = 100
+    N_SAMPLE = 20
+    
+    # merge several dataset together
+    dataset1 = FocusPatchDataset(DIR = 'data/202410041703', imsize = [1600,1200], 
+                                psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
+                                DEPTH_GAP=DEPTH_GAP, transforms = trans)
+    dataset2 = FocusPatchDataset(DIR = 'data/202410041747', imsize = [1600,1200], 
+                                psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
+                                DEPTH_GAP=DEPTH_GAP, transforms = trans)
+    dataset3 = FocusPatchDataset(DIR = 'data/202410080808', imsize = [1600,1200], 
+                                psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
+                                DEPTH_GAP=DEPTH_GAP, transforms = trans)
+    dataset4= FocusPatchDataset(DIR = 'data/202410080819', imsize = [1600,1200], 
+                                psize=PSIZE, channels=2, n_sample=N_SAMPLE, 
+                                DEPTH_GAP=DEPTH_GAP, transforms = trans)
+    
+    dataset = torch.utils.data.ConcatDataset([dataset1,dataset2,dataset3,dataset4])
+
+    # test dataset, train datasetに分割する
+    train_set, test_set = torch.utils.data.random_split(dataset, [0.7,0.3])
+    print("train dataset", len(train_set), "test dataset", len(test_set))
+
+    # データローダーの作成
+    train_loader = torch.utils.data.DataLoader(train_set,
+                                               batch_size=1024,  # バッチサイズ
+                                               shuffle=True,  # データシャッフル
+                                               num_workers=2,  # 高速化
+                                               pin_memory=True  # 高速化                                      
+                                               )
+    test_loader = torch.utils.data.DataLoader(test_set,
+                                              batch_size=1024,
+                                              shuffle=True,
+                                              num_workers=2,  # 高速化
+                                              pin_memory=True  # 高速化                                                   
+                                              )
+
+    # モデル・損失関数・最適化アルゴリスムの設定
+    LOAD_WEIGHT = True
+    # model = RESNETLIKE(channels=2).to(device)
+    model = MyModel(channels=2)
+    #model = MyModel_shallow(channels=2).to(device)
+    weight_file = f"weights/weight_{PSIZE}_{DEPTH_GAP}.pth"
+    
+    if LOAD_WEIGHT == True:
+        # 学習済みモデルのロード
+        print("load weight", weight_file)
+        model.load_state_dict(torch.load(weight_file))
+        model.eval()
+    
+    model = model.to(device)
+    
+    criterion = nn.L1Loss()
+    optimizer = optim.Adam(model.parameters())
+    #optimizer = optim.SGD(model.parameters())
+
     # 訓練の実行
     epoch = 50
     train_loss = []
@@ -250,62 +242,13 @@ if __name__ == "__main__":
         test_loss.append(test_l)    
         # 10エポックごとにロスを表示
         #if epoch % 5 == 0:
-        print(f"Train loss: {train_loss[-1]:.3f}, Test loss: {test_loss[-1]:.3f}")
+        print(f"{epoch}: train loss: {train_loss[-1]:.3f}, test loss: {test_loss[-1]:.3f}")
     
-    # 学習状況（ロス）の確認
-    plt.plot(train_loss, label='train_loss')
-    plt.plot(test_loss, label='test_loss')
-    plt.legend()
-
     # モデルの保存
     print("save weight as: ", weight_file)
     torch.save(model.state_dict(), weight_file)
 
-
-# In[12]:
-
-
-# 学習済みモデルから予測結果と正解値を取得
-def retrieve_result(model, dataloader):
-    model.eval()
-    preds = []
-    labels = []
-    # Retreive prediction and labels
-    with torch.no_grad():
-        for data, label in dataloader:
-            data, label = data.to(device), label.to(device)
-            output = model(data)
-            # Collect data
-            preds.append(output)
-            labels.append(label)
-    # Flatten
-    preds = torch.cat(preds, axis=0)
-    labels = torch.cat(labels, axis=0)
-    # Returns as numpy (CPU環境の場合は不要)
-    preds = preds.cpu().detach().numpy()
-    labels = labels.cpu().detach().numpy()
-    return preds, labels
-
-
-# データを作って評価してみる
-dataset = FocusPatchDataset(DIR = 'data/202410041747', imsize = [1600,1200], 
-                            psize=PSIZE, channels=2, n_sample=1, 
-                            DEPTH_GAP=DEPTH_GAP, transforms = trans)
-test_loader = torch.utils.data.DataLoader(dataset, batch_size=256, shuffle=False)
-preds, labels = retrieve_result(model, test_loader)
-
-
-# In[14]:
-
-
-if __name__ == "__main__":
-    plt.plot(preds, label="prediction")
-    plt.plot(labels, label='ground truth')
+    # 学習状況（ロス）の確認
+    plt.plot(train_loss, label='train_loss')
+    plt.plot(test_loss, label='test_loss')
     plt.legend()
-
-
-# In[ ]:
-
-
-
-
